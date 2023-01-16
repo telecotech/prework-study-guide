51a2d4d99158a7910842c7c6f54be9aad03a5c6e
Mullens <telecotech82@gmail.com>
2023-01-16 10:40:36 -0500

    jiohoi

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,6 @@
 var topic = "HTML";
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
 var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
