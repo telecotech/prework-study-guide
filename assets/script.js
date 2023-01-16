001b5a4f6af61d832e0d81ac320c3db6e86ab8c6
Mullens <telecotech82@gmail.com>
2023-01-16 11:14:04 -0500

    i made a mistake

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-   }
-
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+  function selectTopic() {
+    if (topic === 'HTML') {
+      console.log("Let's study HTML!");
    /*
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
